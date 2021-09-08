74f02f66e6147c6653c5b2744d6aded02665819b
Pmparekh2000 <pmp.tech2000@gmail.com>
2021-09-08 13:22:53 +0530

    Completed UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,10 +28,29 @@ export class AppComponent implements OnInit{
     });
   }
 
-  public onOpenModal(employee: Employee, mode: String): void {
+  // public onOpenModal(employee: Employee, mode: string): void {
+  //   const container = document.getElementById('main-container');
+  //   const button = document.createElement('button');
+  //   // changed the type from submit to button
+  //   button.type = 'button';
+  //   button.style.display = 'none';
+  //   button.setAttribute('data-toggle', 'modal');
+  //   // if (mode === 'add') {
+  //   //   button.setAttribute('data-target', '#addEmployeeModal');
+  //   // }
+  //   if (mode === 'edit') {
+  //     button.setAttribute('data-target', '#updateEmployeeModal');
+  //   }
+  //   // if (mode === 'delete') {
+  //   //   button.setAttribute('data-target', '#deleteEmployeeModal');
+  //   // }
+  //   container.appendChild(button);
+  //   button.click();
+  // }
+
+  public onOpenModal(employee: Employee, mode: string): void {
     const container = document.getElementById('main-container');
     const button = document.createElement('button');
-    // changed the type from submit to button
     button.type = 'button';
     button.style.display = 'none';
     button.setAttribute('data-toggle', 'modal');
@@ -39,9 +58,12 @@ export class AppComponent implements OnInit{
       button.setAttribute('data-target', '#addEmployeeModal');
     }
     if (mode === 'edit') {
-      button.setAttribute('data-target', '#updateEmployeeModal');
+      console.log('Hello');
+      // this.editEmployee = employee;
+      button.setAttribute('data-target', 'updateEmployeeModal');
     }
     if (mode === 'delete') {
+      // this.deleteEmployee = employee;
       button.setAttribute('data-target', '#deleteEmployeeModal');
     }
     container.appendChild(button);
